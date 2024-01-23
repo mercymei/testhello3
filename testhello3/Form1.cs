7c34732fc696b3fa26484c6876c5f23742a6a189
Administrator <Administrator@AFP2>
2024-01-23 17:37:07 +0800

    test

--- testhello3/Form1.cs
+++ testhello3/Form1.cs
@@ -19,7 +19,7 @@ namespace testhello3
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("2222222");
+            MessageBox.Show("testhello3");
         }
     }
 }
